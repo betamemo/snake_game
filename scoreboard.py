473e4845e871a3ad65feb599037415389be1e9f0
BK <betamemo@BK.local>
2023-10-18 10:18:55 +0700

    edit font

--- scoreboard.py
+++ scoreboard.py
@@ -1,16 +1,24 @@
 from turtle import Turtle
 
+FONT = ('verdana', 20, 'normal')
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
         self.penup()
         self.hideturtle()  # hide an arrow
+        self.score = 0
+        self.highest_score = 0
+        self.print_highest_score()
 
     def print_score(self):
-        self.goto(0, 260)
-        self.write(f'Score: {self.score}', align='center', font=('Arial', 25, 'normal'))
+        self.goto(280, 260)
+        self.write(f'Score: {self.score}', align='right', font=FONT)
+
+    def print_highest_score(self):
+        self.goto(-280, 260)
+        self.write(f'Highest Score: {self.highest_score}', align='left', font=FONT)
 
     def add_score(self, score):
         self.score += score
@@ -19,4 +27,4 @@ class Scoreboard(Turtle):
 
     def game_over(self):
         self.goto(0, 0)
-        self.write(f'Game Over', align='center', font=('Arial', 40, 'normal'))
+        self.write(f'Game Over', align='center', font=FONT)
